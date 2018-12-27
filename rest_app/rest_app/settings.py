e48f0af87f694dfea5346e7efbb1bd78ac7121e1
Robert <trobert93@gmail.com>
2018-12-27 10:20:19 +0100

    add viewsets

--- rest_app/rest_app/settings.py
+++ rest_app/rest_app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rental',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
