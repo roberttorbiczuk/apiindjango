8a60613d6c1816e0b3878bd5eabb7db959b0fd67
Robert <trobert93@gmail.com>
2018-12-27 10:37:13 +0100

    fixed import

--- rest_app/rest_app/api.py
+++ rest_app/rest_app/api.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 
-from rest_app.rental.api_views import FriendViewset, BelongingViewset, BorrowedViewset
+from rental.api_views import FriendViewset, BelongingViewset, BorrowedViewset
 
 router = routers.DefaultRouter()
 router.register(r'friends', FriendViewset)
